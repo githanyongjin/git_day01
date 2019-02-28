3f9192e56ef399e24e63f8b5bb62772346b593f2
githanyongjin <893512042@qq.com>
2019-02-28 19:09:04 +0800

    添加git版本1到本地仓库

---
+++ day01_git/src/main/java/cn/itcast/git/GitTest.java
@@ -0,0 +1,16 @@
+package cn.itcast.git;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("大家好，我是git");
+        System.out.println("大家好，我是git");
+        System.out.println("大家好，我是git");
+        System.out.println("大家好，我是git");
+        System.out.println("大家好，我是git");
+    }
+    public void test(){
+        int i = 0;
+
+        System.out.println(i);
+    }
+}
